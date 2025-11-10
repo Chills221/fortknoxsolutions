1bfa50e674c8bed2fd5951a2815e5379523e3c4d
Nalla <nani.rama1982@gmail.com>
2025-11-10 10:12:16 +0530

    Changes homepage

--- src/components/ProtectComponent.jsx
+++ src/components/ProtectComponent.jsx
@@ -16,7 +16,7 @@ const ProtectComponent =()=>{
                 <div className=" flex md:flex-row flex-col gap-20 items-center">
                     <div className="md:w-[55%] w-full flex flex-col gap-4">
                         <h2 className="lg:text-7xl md:text-5xl text-3xl text-[#fcf8ef] leading-[1.2]">Protect What Matters Most.</h2>
-                        <p className="lg:text-3xl md:text-2xl text-xl text-white">Fort Knox Solutions provides to premium security fiilm and door reinforcements to keep your home safe</p>
+                        <p className="lg:text-3xl md:text-2xl text-xl text-white">Fort Knox Solutions provides premium security film and door reinforcements to keep your home safe</p>
                         <div className="inline-flex mt-6">
                         <a href="#contactform"  className="relative inline-block text-center overflow-hidden z-10 text-black hover:text-white hover:bg-black text-sm font-medium bg-[#cfb16b] px-10 uppercase py-3 tracking-[2.64px] transition-all duration-300 ease-linear">Get a Free Quote</a>
                         </div>
